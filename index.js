87d1fc1ac9c0fad765c0a95e1eaff621714abe21
prakash02220 <parkash.mishra@webspero.com>
2025-04-01 12:36:14 +0530

    changing file namesfsd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export {default as MyButton} from './MyButton/MyButton';
-export {default as MyAutoComplete} from './MyAutoComplete/MyAutoComplete';
-export {default as MySearch} from './MySearch/MySearch';
-export {default as MySelect} from './MySelect/MySelect';
-export {default as MySwitch} from './MySwitch/MySwitch';
-export {default as MyTextfield} from './MyTextfield/MyTextfield';
+export {default as MyButton} from './MyButton/MyButton.js';
+export {default as MyAutoComplete} from './MyAutoComplete/MyAutoComplete.js';
+export {default as MySearch} from './MySearch/MySearch.js';
+export {default as MySelect} from './MySelect/MySelect.js';
+export {default as MySwitch} from './MySwitch/MySwitch.js';
+export {default as MyTextfield} from './MyTextfield/MyTextfield.js';
